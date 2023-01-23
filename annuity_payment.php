8f85e8694fd8485e177a4d6567c66e9e88265b69
Ypowerdev <117303538+Ypowerdev@users.noreply.github.com>
2023-01-23 14:08:22 +0300

    Update annuity_payment.php

--- annuity_payment.php
+++ annuity_payment.php
@@ -1,45 +1,117 @@
 <?php
 
-function credit(float $S, float $P, float $N): array
-{
-    $P /= 1200;
-    $N *= 12;
-    $X = $S * $P / (1 - pow(1 + $P, -$N));
-    return [$X, $X * $N - $S];
+class AnnuityPayment
+{ 
+    private float $creditSum; 
+    private float $annualPercentageRate; 
+    private float $creditForYears; 
+    private const MONTHS_IN_YEAR = 12; 
+    private const COEFFICIENT = 1200;
+    private const INIT_NUMBER = 0; 
+    private $testCases = [
+            [
+            'creditSum' => 6000000,
+            'annualPercentageRate' => 8, 
+            'creditForYears' => 10
+            ],
+            [
+            'creditSum' => 5000000,
+            'annualPercentageRate' => 10, 
+            'creditForYears' => 12
+            ],
+            [
+            'creditSum' => 4000000,
+            'annualPercentageRate' => 8, 
+            'creditForYears' => 10
+            ]
+        ];  
+      
+    public function __construct(array $creditData = []) 
+    { 
+        if (!empty($creditData)) { 
+            $this->creditSum = $creditData['creditSum']; 
+            $this->annualPercentageRate = $creditData['annualPercentageRate']; 
+            $this->creditForYears = $creditData['creditForYears'];
+        } else {
+            echo 'Пожалуйста, введите данные в форму!'."\n";
+            echo "</br>"; 
+            echo 'Вот примерные данные!'."\n";
+            echo "</br>"; 
+            
+            $indexTestCreditData = rand(0,2); 
+            $this->creditSum = $this->testCases[$indexTestCreditData]['creditSum'];
+            $this->annualPercentageRate = $this->testCases[$indexTestCreditData]['annualPercentageRate'];
+            $this->creditForYears =  $this->testCases[$indexTestCreditData]['creditForYears'];
+        }
+        
+        $this->creditForMonths = $this->creditForYears * self::MONTHS_IN_YEAR; 
+        $this->percentageCoefficient = $this->annualPercentageRate / self::COEFFICIENT;
+    }
+    
+    public function getForm(): string 
+    { 
+                
+        $form = '
+        <form method="POST">
+        <div class="row">
+            <p>Сумма кредита:<br> <input type="number" name="creditSum" value="'. self::INIT_NUMBER . '"></p>
+        </div>
+        <div class="row">
+        <p>Годовая процентная ставка:<br><input type="number" name="annualPercentageRate" value="'. self::INIT_NUMBER . '"></p>
+        </div>
+        <div class="row">
+            <p>Срок кредитования:<br><input type="number" name="creditForYears" value="' . self::INIT_NUMBER . '"></p>
+        </div>
+        <div class="row">
+            <input type="submit">
+        </div>';
+        
+        $form .= '</form>';
+        return $form; 
+    } 
+    
+    private function getAnnPayment(): float
+    { 
+        
+        $credit1 = $this->creditSum * $this->percentageCoefficient; 
+        $credit2 = 1 - pow(1 + $this->percentageCoefficient, -$this->creditForMonths); 
+        $annuityPayment = $credit1 / $credit2; 
+        return $annuityPayment; 
+    }
+    
+    private function getOverpaymentInfo(): float
+    {
+        $getOverpaymentInfo = $this->getAnnPayment() * $this->creditForMonths - $this->creditSum;
+        return $getOverpaymentInfo; 
+    }  
+    
+    
+    public function printAllCreditInfo(): void 
+    { 
+       echo 'Общая сумма кредита: ' .  $this->creditSum . ' рублей' . '<br>';
+       echo 'Годовая процентная ставка: ' . $this->annualPercentageRate . ' процентов' . '<br>'; 
+       echo 'Срок кредитования: ' . $this->creditForYears . ' лет' . '<br>'; 
+       echo 'Ежемесячный платёж: ' . round ($this->getAnnPayment(), 2) . ' рублей' . '<br>'; 
+       echo 'Переплата: ' . round ($this->getOverpaymentInfo(), 2) . ' рублей' . '<br>'; 
+            
+    }
+    
+    public function printAllCreditInfoHTML(): string 
+    { 
+        $allCreditInfo =           
+            '<p><br> Общая сумма кредита: '. $this->creditSum . ' рублей </p>
+            <p>Годовая процентная ставка: ' . $this->annualPercentageRate . ' процентов </p>
+            <p>Годовая процентная ставка: ' . $this->annualPercentageRate . ' процентов </p> 
+            <p>Срок кредитования: ' . $this->creditForYears . ' лет </p> 
+            <p>Ежемесячный платёж: ' . round ($this->getAnnPayment(), 2) .  ' рублей </p> 
+            <p>Переплата: ' . round ($this->getOverpaymentInfo(), 2) . ' рублей </p>';  
+                    
+        return $allCreditInfo; 
+        
+    }
 }
 
-$credit_sum = 0;
-$annual_percentage_rate = 0;
-$year = 0;
+$credit = new AnnuityPayment($_POST); 
+$credit->printAllCreditInfo(); 
+echo $credit->getForm(); 
 
-if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-    $credit_sum = (float) $_POST['credit_sum'];
-    $annual_percentage_rate = (float) $_POST['annual_percentage_rate'];
-    $year = (float) $_POST['year'];
-
-    list($X, $overpayment) = credit($credit_sum, $annual_percentage_rate, $year);
-
-    print_r("\n<br>Сумма кредита: " . $credit_sum);
-    print_r("\n<br>Годовая процентная ставка: " . $annual_percentage_rate . '%');
-    print_r("\n<br>Срок кредитования: " . $year . ' года');
-    print_r("\n<br>Ежемесячный платеж: " . round($X, 2));
-    print_r("\n<br>Переплата: " . round($overpayment, 2));
-}
-
-?>
-
-
-<form method="POST">
-   <div class="row">
-      <p>Сумма кредита:<br> <input type="number" name="credit_sum" value="<?=$credit_sum?>"></p>
-   </div>
-   <div class="row">
-   <p>Годовая процентная ставка:<br><input type="text" name="annual_percentage_rate" value="<?=$annual_percentage_rate?>"></p>
-   </div>
-   <div class="row">
-   <p>Срок кредитования:<br><input type="number" name="year" value="<?=$year?>"></p>
-   </div>
-   <div class="row">
-     <input type="submit">
-   </div>
-   </form>
